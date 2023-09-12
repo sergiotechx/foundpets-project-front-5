cdec80a1f57146ed06b0f2c551857ef14a3067d3
Mafer <vegapachecofer@gmail.com>
2023-09-12 10:13:03 -0500

    tienda

---
+++ src/app/(pages)/petShop/page.jsx
@@ -0,0 +1,37 @@
+'use client'
+import React from 'react'
+import "./style.scss"
+const page = () => {
+  return (
+    
+    <div className='store'>
+    
+    <div className='store2'>
+    <img src="/images/collar.jpg"/> 
+    <button>hola mundo</button>
+    </div>
+
+    <div className='store2'>
+    <img src="/images/collar.jpg"/> 
+    </div>
+
+    <div className='store2'>
+    <img src="/images/collar.jpg"/> 
+    </div>
+
+    <div className='store2'>
+       <img src="/images/collar.jpg"/> 
+    </div>
+
+    <div className='store2'>
+        <img src="/images/collar.jpg"/> 
+    </div>
+    <div className='store1'>
+    <label htmlFor="">  Mas detalles </label> 
+    </div> 
+    </div>
+    
+  )
+}
+
+export default page
