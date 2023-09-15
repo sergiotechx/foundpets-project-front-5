0e2e904f961253f8d9de870b17f1126b41cb8c17
Alexis Garcia <alexis25yo@gmail.com>
2023-09-15 11:05:33 -0500

    added if in nabvar

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Roboto_Serif } from "next/font/google";
 import Header from "@/components/header/header";
+import { usePathname } from "next/navigation";
 
 const roboto = Roboto_Serif({
   subsets: ["latin"],
@@ -16,6 +17,12 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const currentPath = usePathname();
+  const hiddenHeader = !(
+    currentPath === "/user/login" ||
+    currentPath === "/user/register" ||
+    currentPath === "/user/profile"
+  );
   return (
     <html lang="es">
       <head>
@@ -23,7 +30,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico"></link>
       </head>
       <body className={roboto.className}>
-        <Header />
+        {hiddenHeader && <Header />}
         {children}
       </body>
     </html>
