dc5afa4bb678373a5114bc078b8fb953e41bfb55
sergiotechx <sergiotechx@yahoo.com>
2023-09-28 19:31:15 -0500

    ensayos perrunos

--- src/app/(pages)/testdog/page.jsx
+++ src/app/(pages)/testdog/page.jsx
@@ -1,10 +1,15 @@
-import Dog from '@/components/dog/dog'
-import React from 'react'
 
+
+import React from 'react'
+import Dog from '@/components/dog/dog'
 const page = () => {
   return (
     <div>
+     
+     
+      
       <Dog/>
+  
     </div>
   )
 }
