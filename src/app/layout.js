d79110ef9259b90b790da041b9be4782ef58b850
sergiotechx <sergiotechx@yahoo.com>
2023-10-03 10:20:59 -0500

    mini cambios

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 "use client";
+import './styles/layout.scss'
 import "bootstrap/dist/css/bootstrap.css";
 //import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
