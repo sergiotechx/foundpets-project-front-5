d10f104c1d7800b06cf6162488aba0809a68c8c3
Sergio  Martinez <sergiotechx@yahoo.com>
2024-05-30 20:44:25 -0500

    Bogota

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -6,6 +6,7 @@ export const cities = [
   { label: "Bello", value: 2 },
   { label: "Itagui", value: 3 },
   { label: "Barbosa", value: 4 },
+  { label: "Bogota", value: 5 },
 ];
 
 export const species = [
